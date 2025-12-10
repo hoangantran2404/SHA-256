41b625222e74bee8bbe05dc1d57b8dd36ece2fdb
Ngo Tran Hoang An <ngotranhoangan2007@gmail.com>
2025-12-09 21:48:08 -0600

    Refactor SHA-256 to use input_block and update includes
    Updated the SHA-256 implementation to include a new input block and modified the message expander function call.

--- Embedded Software/SHA_256.c
+++ Embedded Software/SHA_256.c
@@ -1,29 +1,11 @@
+// I desgin this file to compute the hash value via software and then verify it on testbench and waveform.
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include "MEvMC.c"
+#include "ME.c"
 
 int main(){
-// Message Expander
-
-    uint32_t message [16] ={
-        0x61626380, 0x00000000, 0x00000000, 0x00000000, // "abc" + 1 padding bit
-		0x00000000, 0x00000000, 0x00000000, 0x00000000,
-		0x00000000, 0x00000000, 0x00000000, 0x00000000,
-		0x00000000, 0x00000000, 0x00000000, 0x00000018 
-    };
-
-    uint32_t W[64];
-    ME_c(message,W);
-
-    printf("ME(\"abc\")= \n");
-    for (int i = 0; i < 64; i++)
-    {
-        printf("W[%02d] = %08x\n ",i, W[i]);
-    }
-    return 0;
-
-// Message Compression
     uint32_t state[8] ={
         0x6a09e667,
         0xbb67ae85,
@@ -34,75 +16,30 @@ int main(){
         0x1f83d9ab,
         0x5be0cd19
     };
-    const uint32_t message[64] ={    // "abc"
-        0x61626380,
+    const uint32_t input_block[16] ={   
+        0x53656375, // "Secu"
+        0x72652048, // "re H"
+        0x61736820, // "ash "
+        0x416C676F, // "Algo"
+        0x72697468, // "rith"
+        0x6D203235, // "m 25"
+        0x36800000, // "6" + Padding (0x80) + 0x00...
+        0x00000000, // Padding zeros
         0x00000000,
         0x00000000,
         0x00000000,
         0x00000000,
         0x00000000,
         0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000000,
-        0x00000018,
-        0x61626380,
-        0x000f0000,
-        0x7da86405,
-        0x600003c6,
-        0x3e9d7b78,
-        0x0183fc00,
-        0x12dcbfdb,
-        0xe2e2c38e,
-        0xc8215c1a,
-        0xb73679a2,
-        0xe5bc3909,
-        0x32663c5b,
-        0x9d209d67,
-        0xec8726cb,
-        0x702138a4,
-        0xd3b7973b,
-        0x93f5997f,
-        0x3b68ba73,
-        0xaff4ffc1,
-        0xf10a5c62,
-        0x0a8b3996,
-        0x72af830a,
-        0x9409e33e,
-        0x24641522,
-        0x9f47bf94,
-        0xf0a64f5a,
-        0x3e246a79,
-        0x27333ba3,
-        0x0c4763f2,
-        0x840abf27,
-        0x7a290d5d,
-        0x065c43da,
-        0xfb3e89cb,
-        0xcc7617db,
-        0xb9e66c34,
-        0xa9993667,
-        0x84badedd,
-        0xc21462bc,
-        0x1487472c,
-        0xb20f7a99,
-        0xef57b9cd,
-        0xebe6b238,
-        0x9fe3095e,
-        0x78bc8d4b,
-        0xa43fcf15,
-        0x668b2ff8,
-        0xeeaba2cc,
-        0x12b1edeb
+        0x00000000, // Length (High 32-bit)
+        0x000000C8, // Length (Low 32-bit) = 200 bits
     };
-    MC(state,message);
+    uint32_t W[64];
+    ME_c(input_block,W);
 
-    printf("MC(\"abc\")= \n");
+    MC(state,W);
+
+    printf("Secure Hash Algorithm 256= \n");
     for (int i = 0; i < 8; i++)
     {
         printf("State[%02d] = %08x\n ",i, state[i]);
